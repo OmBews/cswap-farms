9626bd9d6be66365cdabc4c3ab1ae472df227032
Shahzeb Khan <shahzeb8285@gmail.com>
2021-07-17 02:42:02 +0530

    first commit

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,46 @@
+export default {
+  cake: {
+    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    97: '',
+    137: '0x3FaF2f0498F09D6c0678f70138311Ae15c048fA9', // CSWAP
+  },
+  masterChef: {
+    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    97: '',
+    137: '0x9eA89058184816c5e3Beb573D28409B378EDBA97' 
+  },
+  sousMasterChef: {
+    56: '',
+    97: '',
+    137: '0xBA13f14f73e88A2Aa264EC3063b32f4b4972E226'
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+    137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270' // WMATIC
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+    137: '0x95028E5B8a734bb7E2071F96De89BABe75be9C8E',
+  },
+  busd: {
+    56: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+    97: '',
+    137: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174' // USDC
+  },
+
+  referral:{
+    56: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+    97: '',
+    137: '0xfe71261e218c8d5aad90dc322e25ed7b4f2d5d50' // USDC
+  }
+}
